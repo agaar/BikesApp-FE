26e9eae130724ed4a6f112805235c1bb6e07218f
agar91 <agnieszkarapa@gmail.com>
2021-03-23 11:25:57 +0100

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   audience: 'http://localhost:8091',
-  redirectUri: 'https://raga-intro-bikes.herokuapp.com/callback'
+  redirectUri: 'https://raga-intro-bikes-fe.herokuapp.com/callback'
 };
