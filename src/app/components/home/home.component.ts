6d7f34c26546702a39bbd489e734ac8efe1d04fc
agar91 <agnieszkarapa@gmail.com>
2021-03-26 11:25:26 +0100

    validation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -48,20 +48,8 @@ export class HomeComponent implements OnInit {
       )
     }
     else {
-      this.validateAllFormFields(this.bikeform);
-      this.validMessage = "Please fill out the form before submitting !"
+         this.validMessage = "Please fill out the form before submitting !"
     }
   }
-
-  validateAllFormFields(formGroup: FormGroup) {         //{1}
-  Object.keys(formGroup.controls).forEach(field => {  //{2}
-    const control = formGroup.get(field);             //{3}
-    if (control instanceof FormControl) {             //{4}
-      control.markAsTouched({ onlySelf: true });
-    } else if (control instanceof FormGroup) {        //{5}
-      this.validateAllFormFields(control);            //{6}
-    }
-  });
-}
 
 }
